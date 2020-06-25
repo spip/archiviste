bc208683e662c116f9a3d760416791854b8d7f98
Matthieu Marcillaud <marcimat@rezo.net>
2020-06-25 19:46:57 +0200

    Notice PHP en moins en dehors de SPIP.

--- inc/pclzip.php
+++ inc/pclzip.php
@@ -5629,7 +5629,10 @@ function PclZipUtilOptionText($p_option) {
 //   The path translated.
 // --------------------------------------------------------------------------------
 function PclZipUtilTranslateWinPath($p_path, $p_remove_disk_letter = true) {
-	if (_OS_SERVEUR == 'windows' or stristr(@php_uname(), 'windows')) {
+	if (
+		(defined('_OS_SERVEUR') and _OS_SERVEUR === 'windows')
+		or stristr(@php_uname(), 'windows')
+	) {
 		// ----- Look for potential disk letter
 		if (($p_remove_disk_letter) && (($v_position = strpos($p_path, ':')) != false)) {
 			$p_path = substr($p_path, $v_position + 1);
