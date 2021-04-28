93f35293ab3cb9533ec2b629f7131477404e8f62
Ricardo Porto <rporto@calepino.com>
2021-04-29 01:27:03 +0200

    [Salvatore] [source:lang/ paquet-archiviste] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ paquet-archiviste] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-archiviste_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-archiviste?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'archiviste_description' => 'Permite comprimir ou expandir arquivos Zip, Tar, ...',
+	'archiviste_nom' => 'Arquivista',
+	'archiviste_slogan' => 'Gerenciar arquivos compactados'
+);
