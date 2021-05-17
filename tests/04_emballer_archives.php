9e5d11ed2a5ebddbddd732a9d9b0de6785eb8a76
Cerdic <cedric@yterium.com>
2021-05-17 17:10:28 +0200

    Tester emballer() sur tous les formats connus

--- tests/04_emballer_archives.php
+++ tests/04_emballer_archives.php
@@ -20,25 +20,28 @@ archiviste_nettoyer_environnement_test();
 
 include_spip('inc/archives');
 
-$fichier = archiviste_fichier_de_test('zip');
-$files_list = archiviste_generer_contenu_de_test(archiviste_contenu_de_test());
 $destination = archiviste_repertoire_de_test() . '/';
 
-$archive = new SpipArchives($fichier);
-if (!$archive->emballer($files_list)) {
-	var_dump($archive->erreur(),$archive->message());
-	archiviste_finir_test("Echec emballer ".json_encode($files_list), $destination);
+foreach (SpipArchives::compressionsConnues as $format){
+
+	$fichier = archiviste_fichier_de_test($format);
+	$files_list = archiviste_generer_contenu_de_test(archiviste_contenu_de_test());
+
+	$archive = new SpipArchives($fichier);
+	if (!$archive->emballer($files_list)){
+		var_dump($archive->erreur(), $archive->message());
+		archiviste_finir_test("[$format] Echec emballer " . json_encode($files_list), $destination);
+	}
+
+	archiviste_nettoyer_contenu_de_test(archiviste_contenu_de_test(), $destination);
+
+	$infos = $archive->informer();
+	if (!$infos or count($infos['fichiers'])!==count($files_list)){
+		var_dump($infos);
+		archiviste_finir_test("[$format] Echec emballer : nombre de fichiers incorrects", $destination);
+	}
+
+	archiviste_teste_deballer($fichier, $format);
 }
-
-archiviste_nettoyer_contenu_de_test(archiviste_contenu_de_test(), $destination);
-
-$infos = $archive->informer();
-if (!$infos or count($infos['fichiers']) !== count($files_list)) {
-	var_dump($infos);
-	archiviste_finir_test("Echec emballer : nombre de fichiers incorrects", $destination);
-}
-
-archiviste_teste_deballer($fichier);
-
 
 archiviste_finir_test(false, $destination);
