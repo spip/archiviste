c44a4ea68888aec28f4dae46a57c6a59a948fd33
JamesRezo <james@rezo.net>
2022-02-02 13:05:22 +0100

    Version 2.1.0

--- lang/archiviste_fr.php
+++ lang/archiviste_fr.php
@@ -1,12 +1,12 @@
 <?php
 
 $GLOBALS[$GLOBALS['idx_lang']] = [
-    'OK' => 'OK',
-    'erreur_inconnue' => 'Erreur inconnue',
-    'extension_inconnue' => 'Extension inconnue',
-    'fichier_absent' => 'Fichier absent',
-    'fichier_lecture_seule' => 'Fichier en lecture seule',
-    'destination_inaccessible' => 'Desination inaccessible',
-    'fichier_deja_existant' => 'Le fichier existe d&eacute;j&agrave;',
+	'OK' => 'OK',
+	'erreur_inconnue' => 'Erreur inconnue',
+	'extension_inconnue' => 'Extension inconnue',
+	'fichier_absent' => 'Fichier absent',
+	'fichier_lecture_seule' => 'Fichier en lecture seule',
+	'destination_inaccessible' => 'Desination inaccessible',
+	'fichier_deja_existant' => 'Le fichier existe d&eacute;j&agrave;',
 	'tentative_de_vidage_du_fichier' => 'Impossible de vider totalement un fichier d\'archive',
 ];
