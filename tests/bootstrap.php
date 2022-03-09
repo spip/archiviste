6b2b231c347ac120d0f71d157bbe6557ae5fada0
Cerdic <cedric@yterium.com>
2022-03-09 17:39:52 +0100

    un bootstrap qui fonctionne aussi si on lance la suite de tests/ generale sans avoir fait un composer install sur ce plugin archiviste

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 // si on lance les tests depuis tests/ dans une installation SPIP ...
 if (function_exists('include_spip')) {
     include_spip('inc/archives');
+}
+else {
+	require_once __DIR__ . '/../vendor/autoload.php';
 }
